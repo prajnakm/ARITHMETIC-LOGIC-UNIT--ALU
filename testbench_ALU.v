92573907a8ec830ca520b1fbdfd955362ca598d2
prajnakm <roxy71791@gmail.com>
2026-05-12 22:45:04 +0530

    Update testbench_ALU.v

--- testbench_ALU.v
+++ testbench_ALU.v
@@ -1,24 +1,4 @@
 `timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 06.05.2026 22:50:35
-// Design Name: 
-// Module Name: testbench_ALU
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
-
 
 module testbench_ALU;
 
