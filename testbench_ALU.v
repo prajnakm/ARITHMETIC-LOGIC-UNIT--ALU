89b797835468627e1cec95b1f466e45a96544348
prajnakm <roxy71791@gmail.com>
2026-05-12 23:31:24 +0530

    Update testbench_ALU.v

--- testbench_ALU.v
+++ testbench_ALU.v
@@ -37,3 +37,10 @@ initial begin
 end
 
 endmodule
+
+//The alu_tb.v file contains the testbench used to verify the functionality of the ALU module. 
+//It creates input signals for the ALU and observes the generated output during simulation.
+//The inputs are declared as reg because their values are changed inside the testbench, while the output is declared as wire since it receives data from the ALU module.
+//The ALU module is instantiated inside the testbench and connected to the test signals.
+//Different input combinations and select values are applied sequentially using an initial block along with time delays (#10).
+//The $display and $monitor statements are used to print input and output values during simulation for easier verification of the ALU operations.
