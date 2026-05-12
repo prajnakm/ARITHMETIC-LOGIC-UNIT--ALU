f4b34ebd44d49e1040d30dce3684435d820e002d
prajnakm <roxy71791@gmail.com>
2026-05-12 22:43:11 +0530

    Update ALU.v

--- ALU.v
+++ ALU.v
@@ -1,24 +1,4 @@
 `timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 04.05.2026 21:35:34
-// Design Name: 
-// Module Name: ALU
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
-
 
 module ALU (
     input [3:0] A,
