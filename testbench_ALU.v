b98bb7bc4102da5643771a0e5274d5dad1d7f17c
prajnakm <roxy71791@gmail.com>
2026-05-12 22:43:54 +0530

    Add files via upload

---
+++ testbench_ALU.v
@@ -0,0 +1,59 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 06.05.2026 22:50:35
+// Design Name: 
+// Module Name: testbench_ALU
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module testbench_ALU;
+
+reg [3:0] A, B;
+reg [2:0] sel;
+wire [3:0] result;
+
+// Instantiate ALU
+ALU uut (
+    .A(A),
+    .B(B),
+    .sel(sel),
+    .result(result)
+);
+
+initial begin
+    $display("A B sel | Result");
+    $monitor("%b %b %b | %b", A, B, sel, result);
+
+    // Test ADD
+    A = 4'b0101; B = 4'b0011; sel = 3'b000; #10;
+
+    // Test SUB
+    A = 4'b0101; B = 4'b0011; sel = 3'b001; #10;
+
+    // Test AND
+    A = 4'b0101; B = 4'b0011; sel = 3'b010; #10;
+
+    // Test OR
+    A = 4'b0101; B = 4'b0011; sel = 3'b011; #10;
+
+    // Test NOT
+    A = 4'b0101; sel = 3'b100; #10;
+
+    $finish;
+end
+
+endmodule
